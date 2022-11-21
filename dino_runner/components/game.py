bd581156c78704e9311b4d7418f4ff590c08afc2
Gui Kimura <satogui5+github@gmail.com>
2022-11-21 20:14:12 -0300

    AULA 1: Class Dinosaur

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
@@ -14,6 +14,7 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,18 +31,20 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
-
+        user_input = pygame.key.get_pressed() # Pega a tecla pressionada
+        self.player.update(user_input)
+        
     def draw(self):
-        self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.clock.tick(FPS) # Ele calculará quantos milissegundos se passaram desde a chamada anterior
+        self.screen.fill((255, 255, 255)) # Preencher a tela
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
-        pygame.display.flip()
+        pygame.display.flip() # Atualize a superfície de exibição completa para a tela
 
     def draw_background(self):
         image_width = BG.get_width()
-        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg)) # Enfileira as imagens
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
