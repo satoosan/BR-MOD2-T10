aa4319f38462c45a608f84476c503cc36acc852f
Gui Kimura <satogui5+github@gmail.com>
2022-11-22 17:14:28 -0300

    Exercicio DINOSAUR DUCKING
    Implementando o metodo duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,11 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+import time
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
+TIME = time.sleep(3)
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = Y_POS + 30
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -15,11 +18,18 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
+        self.step_index += 1
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
 
     def jump(self):
@@ -33,21 +43,28 @@ class Dinosaur:
             self.dino_jump = False
             self.jump_vel = JUMP_VEL
 
-    # A fazer
-    def duck(self):
-        pass
-
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
+        
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
-            self.dino_jump = True
-            self.dino_run = False
-        elif not self.dino_jump:
+        if user_input[pygame.K_UP]:
+            if not self.dino_jump:
+                self.dino_jump = True
+                self.dino_duck = False
+                self.dino_run = False
+        elif user_input[pygame.K_DOWN]:
+            if not self.dino_jump:
+                self.dino_jump = False
+                self.dino_duck = True
+                self.dino_run = False
+        elif not (self.dino_jump or user_input[pygame.K_DOWN]):
             self.dino_jump = False
+            self.dino_duck = False
             self.dino_run = True
 
         if self.step_index >= 10:
